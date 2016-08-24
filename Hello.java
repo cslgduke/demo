89c2c6465bd28a0aa523fd11c848cd306940367f
duke <dk111241@alibaba-inc.com>
2016-08-24 22:10:06 +0800

    delete Helloworld

--- Hello.java
+++ Hello.java
@@ -2,6 +2,5 @@ public class Hello{
     public static void main(String[] args){
         System.out.println("Hello");
         System.out.println("World");
-        System.out.println("HelloWorld");
     }
 }
